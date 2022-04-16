730dfd84b1733cd2bac00ae4a2c5097550773967
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-04-16 11:20:53 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 $(function () {
+    // Carousel play controls
     $(".carousel").carousel({ interval: 2000 });
     $("#carouselButton").click(function () {
-      // When button is clicked check if child node has the pause font awesome pause class
-      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-        // It has the class "fa-pause" so we know the carousel is playing
-        // and we can pause the carousel
-        $(".carousel").carousel("pause");
-        // Change icon on button to play button
-        $("#carouselButton").children("i").removeClass("fa-pause");
-        $("#carouselButton").children("i").addClass("fa-play");
-      } else {
-        // Unpause carousel
-        $(".carousel").carousel("cycle");
-        // Change icon on button to pause button
-        $("#carouselButton").children("i").removeClass("fa-play");
-        $("#carouselButton").children("i").addClass("fa-pause");
-      }
+        // When button is clicked check if child node has the pause font awesome pause class
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            // It has the class "fa-pause" so we know the carousel is playing
+            // and we can pause the carousel
+            $(".carousel").carousel("pause");
+            // Change icon on button to play button
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            // Unpause carousel
+            $(".carousel").carousel("cycle");
+            // Change icon on button to pause button
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
     });
-  });
+
+    // Reserve Campsite modal
+    $("#reserveButton").click(function() {
+        $('#reserveModal').modal('show');
+    })
+    // Login modal
+    $("#loginButton").click(function() {
+        $('#loginModal').modal('show');
+    })
+});
